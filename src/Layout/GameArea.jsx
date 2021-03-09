e1d62d91ae3634727c185618a210b20e457b83c0
Paul Miller <usa.paul@icloud.com>
2021-03-08 20:38:49 -0800

    Add UI component card

--- src/Layout/GameArea.jsx
+++ src/Layout/GameArea.jsx
@@ -23,19 +23,19 @@ export const GameArea = ({ showHistory }) => {
                         <p className="history-item-text_won playerX-title">X -&gt; WON</p>
                     </li>
                     <li className="history-item">
-                        <p className="history-item-text_title">Round 2</p>
-                        <div className="history-item-details">
-                            <p>1: X <span>-&gt;</span> <span>(-1; 1)</span></p>
-                            <p>2: O <span>-&gt;</span> <span>(0; 0)</span></p>
-                            <p>3: X <span>-&gt;</span> <span>(1; 1)</span></p>
-                            <p>4: O <span>-&gt;</span> <span>(1; -1)</span></p>
-                            <p>5: X <span>-&gt;</span> <span>(-1; -1)</span></p>
-                            <p>6: O <span>-&gt;</span> <span>(-1; 0)</span></p>
-                            <p>7: X <span>-&gt;</span> <span>(1; 0)</span></p>
-                            <p>8: O <span>-&gt;</span> <span>(0; 1)</span></p>
-                            <p>9: X <span>-&gt;</span> <span>(0; -1)</span></p>
-                        </div>
-                        <p className="history-item-text_won playerO-title">O -&gt; WON</p>
+                            <p className="history-item-text_title">Round 2</p>
+                            <div className="history-item-details">
+                                <p>1: X <span>-&gt;</span> <span>(-1; 1)</span></p>
+                                <p>2: O <span>-&gt;</span> <span>(0; 0)</span></p>
+                                <p>3: X <span>-&gt;</span> <span>(1; 1)</span></p>
+                                <p>4: O <span>-&gt;</span> <span>(1; -1)</span></p>
+                                <p>5: X <span>-&gt;</span> <span>(-1; -1)</span></p>
+                                <p>6: O <span>-&gt;</span> <span>(-1; 0)</span></p>
+                                <p>7: X <span>-&gt;</span> <span>(1; 0)</span></p>
+                                <p>8: O <span>-&gt;</span> <span>(0; 1)</span></p>
+                                <p>9: X <span>-&gt;</span> <span>(0; -1)</span></p>
+                            </div>
+                            <p className="history-item-text_won playerO-title">O -&gt; WON</p>
                     </li>
                     <li className="history-item"><p>Round 3</p><p>O -&gt; WON</p></li>
                     <li className="history-item"><p>Round 4</p><p>X -&gt; WON</p></li>
--- src/UI_Components/index.js
+++ src/UI_Components/index.js
@@ -2,6 +2,7 @@ import { Box } from './Box';
 import { Sign } from './Sign';
 import { Button } from './Button';
 import { PlayerResult } from './PlayerResult';
+import { Card } from './Card';
 
 
-export { Box, Sign, Button, PlayerResult };
+export { Box, Sign, Button, PlayerResult, Card };
